fdc9aeb410222e5e68ccd47d02f467a25be9ba5d
Rolivhuwa-Thomoli <rolivhuwathomoli@gmail.com>
2025-02-25 17:09:09 +0200

    Adding BankAccount module with a class

--- BankAccount/src/Main.java
+++ BankAccount/src/Main.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        Scanner inputobj = new Scanner(System.in);
+        //----create new object of type BankAccount----------------------------
         BankAccount obj1 = new BankAccount();
-        obj1.setName("Rolivhuwa");
-        obj1.setAccNumber(747392);
-        obj1.deposit(100);
+        //---------prompt-----------------------------
+        System.out.print("Enter account name: ");
+        String name = inputobj.next();
+        System.out.print("Enter account number: ");
+        int accountnum = inputobj.nextInt();
+        System.out.print("Enter deposit: ");
+        double deposit = inputobj.nextDouble();
+        //----Initialize object-------------------------------------------------
+        obj1.setName(name);
+        obj1.setAccNumber(accountnum);
+        obj1.deposit(deposit);
+        //-----current state of object--------------------------------------
+        System.out.println();
         System.out.println("The account has the following details");
         System.out.println("name : " + obj1.getName() + " account number: " + obj1.getAccNumber() + " Balance : "+ obj1.getAccBalance());
         obj1.deductMonthlyFee();
         obj1.explainAccountPolicy();
+        System.out.println();
         System.out.println("After deducting monthly fee");
+        //------state of object after deduction-------------------------------
         System.out.println("The account has the following details");
         System.out.println("name : " + obj1.getName() + " account number: " + obj1.getAccNumber() + " Balance : "+ obj1.getAccBalance());
     }
